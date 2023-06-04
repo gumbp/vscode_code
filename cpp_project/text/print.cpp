bc11425ede69b87f552d4cf7c0c3d3d494501d11
XingHui <19825089231@163.com>
2023-06-04 23:32:08 +0800

    恢复了误删的文件并同步

---
+++ cpp_project/text/print.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int mul(int &a, int &b){
+	return a*b;
+}
+
+int add(int &a,int &b){
+	return a+b;
+}
+int sub(int &a,int&b){
+	return a-b;
+}
+
+
+
+int main(){
+	int a,b;
+	cin>>a>>b;
+	cout<<a<<"*"<<b<<"="<<mul(a,b)<<endl;
+	cout<<a<<"+"<<b<<"="<<add(a,b)<<endl;
+	cout<<a<<"-"<<b<<"="<<sub(a,b)<<endl;
+	return 0;
+}
